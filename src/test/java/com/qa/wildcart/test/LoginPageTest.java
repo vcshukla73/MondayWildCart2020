7932e1c9cd456df35d08fa7ef312245034cf7be1
vcshukla73 <63424788+vcshukla73@users.noreply.github.com>
2021-01-23 00:18:23 -0500

    add login page

--- src/test/java/com/qa/wildcart/test/LoginPageTest.java
+++ src/test/java/com/qa/wildcart/test/LoginPageTest.java
@@ -29,7 +29,7 @@ public class LoginPageTest extends BaseTest {
 	@Severity(SeverityLevel.CRITICAL)
 	@Test(priority = 1)
 	public void verifySignUpLinkTest() {
-
+		System.out.println("running login page");
 		Assert.assertEquals(loginpage.isSignUpButtonExists(), true);
 
 	}
